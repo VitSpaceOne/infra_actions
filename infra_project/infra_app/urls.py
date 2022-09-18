e8119ea49cfaebb39609c0f733fcfbc2d102d95e
Красильников Виталий <vitalces@yandex.ru>
2022-09-18 20:49:57 +0500

    add test 15 with flake8,  test, push on Docker, push on w-server, bot

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'infra_app'  # new comment 1
+app_name = 'infra_app'  # new comment 11
 
 urlpatterns = [
     path('', views.index, name='index'),
